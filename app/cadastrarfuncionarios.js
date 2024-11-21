1966fd1935a58b3344845acc2de43ce768b36928
Igor Rodrigues <151189751+iggor11@users.noreply.github.com>
2024-11-21 15:26:29 -0300

    Add files via upload

---
+++ app/cadastrarfuncionarios.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.getElementById("CadastrarFuncionario").addEventListener("click", function(event) {
+        event.preventDefault();
+
+
+        document.getElementById("CadastrarFuncionario").scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
